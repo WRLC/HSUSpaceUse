72b41f1f56c7b86729519418f209cfd59aec302a
Benmoony <benmiller92@gmail.com>
2018-06-18 10:49:03 -0700

    Added Base Framework for Plugin for range of query selects
    Added bootstrap transfer plugin on the query-range, images and
    javascript enabled to make it work

--- source/query-range.php
+++ source/query-range.php
@@ -13,7 +13,7 @@
     <title> Library Query Report </title>
     <meta charset="utf-8" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
     integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
     crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
@@ -21,58 +21,85 @@
     crossorigin=""></script>
 	<script src="./javascript/report-objs-pop.js"></script>
 	<script src="./javascript/leaflet.rotatedMarker.js"></script>
+
+    <!--scripts here are for the transfer select plugin-->
+    <script src="./javascript/transfer-select-js/jquery.js"></script>
+    <script src="./javascript/transfer-select-js/bootstrap-transfer.js"></script>
+    
+
 	<script src="./javascript/icons.js"></script>
+    
+    <link href="styles/bootstrap-transfer.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/layout.css" type="text/css" >
     <link rel="stylesheet" href="styles/format.css" type="text/css" >
-</head>
-<script type="text/javascript">
 
-    var cur_selected_date;
+    <!-- This script needs to be changed to period/not date after periods are enabled in the database-->
+    <script type="text/javascript">
+        var cur_selected_date;
+        $(function(){
+            $('#date-select').on("change", function(){
+                var form_info = document.getElementById("choose_survey_form");
+                cur_selected_date = form_info.elements["date-select"].value;
 
-    $(function(){
-        $('#date-select').on("change", function(){
-            var form_info = document.getElementById("choose_survey_form");
-            cur_selected_date = form_info.elements["date-select"].value;
-
-            //Get rid previous select options before repopulating 
-            var select = document.getElementById('survey_id_select');
-            var length = select.options.length;
-            if(length > 1){
-                for(i = 0; i < length; i++){
-                    select.remove(1);
-                }
-            }
-			$.ajax({
-                url: 'phpcalls/get-survey-ids.php',
-                type: 'get',
-                data:{ 'selected_date': cur_selected_date },
-                success: function(data){
-
-                    console.log("got dates");
-                    var json_object = JSON.parse(data);
-                    var survey_select = document.getElementById('survey_id_select');
-
-                    for(var i = 0; i < json_object.length; i++){
-                        var obj = json_object[i];
-                        surv_id = obj['survey_id'];
-                        lay_id = obj['layout_id'];
-                        var option = document.createElement('option');
-                        option.value = surv_id;
-                        option.innerHTML = "Survey: " + surv_id +" for Layout" + lay_id;
-                        survey_select.appendChild(option);
+                //Get rid previous select options before repopulating 
+                var select = document.getElementById('survey_id_select');
+                var length = select.options.length;
+                if(length > 1){
+                    for(i = 0; i < length; i++){
+                        select.remove(1);
                     }
                 }
+                $.ajax({
+                    url: 'phpcalls/get-survey-ids.php',
+                    type: 'get',
+                    data:{ 'selected_date': cur_selected_date },
+                    success: function(data){
+
+                        console.log("got dates");
+                        var json_object = JSON.parse(data);
+                        var survey_select = document.getElementById('survey_id_select');
+
+                        for(var i = 0; i < json_object.length; i++){
+                            var obj = json_object[i];
+                            surv_id = obj['survey_id'];
+                            lay_id = obj['layout_id'];
+                            var option = document.createElement('option');
+                            option.value = surv_id;
+                            option.innerHTML = "Survey: " + surv_id +" for Layout" + lay_id;
+                            survey_select.appendChild(option);
+                        }
+                    }
+                });
+                
             });
-			
         });
-    });
-</script>
+    </script>
+
+    <!--script for enabling the bootstrapTransfer plugin-->
+    <script type="text/javascript">
+        $(function() {
+            var t = $('#multi-select').bootstrapTransfer(
+                {'target_id': 'multi-select-input',
+                 'height': '15em',
+                 'hilite_selection': true});
+            
+            t.populate([
+                {value:"1", content:"Apple"},
+                {value:"2", content:"Orange"},
+                {value:"3", content:"Banana"},
+                {value:"4", content:"Peach"},
+                {value:"5", content:"Grapes"}
+            ]);
+
+        });
+    </script>
+
+</head>
+
 <body>
     <header>
         <img class="logo" src="images/hsu-wm.svg">
         <h1>Library Data Collector</h1>
-    
-    
         <?php
             if (!array_key_exists("username", $_SESSION)){
                 ?>
@@ -104,99 +131,22 @@
                     //get_period_options();
                     ?>
                 </select>
-
-                <select name="in-survey's" id="in_survey_select">
+                <select name="in-floor" id="in_survey_select">
                     <!-- Options to be generated by php or javascript from selected period information-->
                     <!-- This will be built with a multiple select with checkboxes-->
                     <option value="0">Choose Survey's</option>
                 </select>
                 <input type="submit" name="submit-query" />
-                
             </fieldset>
         </form>
                 <?php
             }
         ?>
-		
-		<div id="mapid"></div>
-		<div id="reportDiv"></div>
+
+        <div id="multi-select"></div>
+
 		<?php
         
-		if (array_key_exists("survey_id", $_GET)){
-			?>
-			<script>
-				//create maps and grab survey_id
-				var survey_id = <?= $_GET["survey_id"] ?>;
-				areaMap = new Map();
-				furnMap = new Map();
-				
-				//define objects
-				function Area(area_id, verts, area_name){
-					this.area_id = area_id;
-					this.verts = verts;
-					this.area_name = area_name;
-					this.occupants = 0;
-					this.seats = 0;
-				}
-				function Verts(x,y, order){
-					this.x = x;
-					this.y = y;
-					this.order = order;
-				}
-				function Furniture(fid, numSeats, x, y, degreeOffset, ftype, inArea, occupants, activities){
-					this.fid = fid;
-					this.numSeats = numSeats;
-					this.x = x;
-					this.y = y;
-					this.degreeOffset = degreeOffset;
-					this.ftype = ftype;
-					this.inArea = inArea;
-					this.occupants = occupants;
-					this.activities = activities;
-				}
-				function Activity(count, name){
-					this.count = count;
-					this.name = name;
-				}
-				
-				//popuate furnMap and areaMap then place on map
-				populateObjs(survey_id);
-			
-			
-			//make map
-			var mymap = L.map('mapid', {crs: L.CRS.Simple});
-			var areaLayer = L.layerGroup().addTo(mymap);
-			var furnitureLayer = L.layerGroup().addTo(mymap);
-			var bounds = [[0,0], [360,550]];
-
-			mymap.fitBounds(bounds);
-			
-			
-			
-			//On zoomend, resize the marker icons
-        mymap.on('zoomend', function() {
-            var markerSize;
-            //resize the markers depending on zoomlevel so they appear to scale
-            //zoom is limited to 0-4
-            switch(mymap.getZoom()){
-                case 0: markerSize= 5; break;
-                case 1: markerSize= 10; break;
-                case 2: markerSize= 20; break;
-                case 3: markerSize= 40; break;
-                case 4: markerSize= 80; break;
-            }
-            //alert(mymap.getZoom)());
-            var newzoom = '' + (markerSize) +'px';
-            var newLargeZoom = '' + (markerSize*2) +'px';
-            $('#mapid .furnitureIcon').css({'width':newzoom,'height':newzoom});
-            $('#mapid .furnitureLargeIcon').css({'width':newLargeZoom,'height':newLargeZoom});          
-        });
-			
-			
-			
-			</script>
-			<?php
-		}
     ?>
     </main>
     <footer>
