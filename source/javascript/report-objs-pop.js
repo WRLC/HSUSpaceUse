4349d47c33d91ab7ea2561e7a1812e6c9f2ee46b
Eric Mott <ericmott10@gmail.com>
2018-07-16 16:00:26 -0700

    Moved the JS from query-report.php to report-objs-pop.js

--- source/javascript/report-objs-pop.js
+++ source/javascript/report-objs-pop.js
@@ -1,5 +1,308 @@
+//Populates the dropdown menus for selecting a survey
+//Creates the map objects 
 //Gets record information and sends queries for furniture and area which call populates to their maps.
 var report_added = false;
+var cur_selected_date;
+var cur_year;
+var cur_month;
+var cur_day;
+var json_object;
+var survey_info_legend = L.control();
+
+//define objects
+function Area(area_id, verts, area_name){
+	this.area_id = area_id;
+	this.verts = verts;
+	this.area_name = area_name;
+	this.occupants = 0;
+	this.seats = 0;
+}
+
+function Verts(x, y, order){
+	this.x = x;
+	this.y = y;
+	this.order = order;
+}
+
+function Furniture(fid, numSeats, x, y, degreeOffset, ftype, inArea, occupants, activities){
+	this.fid = fid;
+	this.numSeats = numSeats;
+	this.x = x;
+	this.y = y;
+	this.degreeOffset = degreeOffset;
+	this.ftype = ftype;
+	this.inArea = inArea;
+	this.occupants = occupants;
+	this.activities = activities;
+	totalSeats = +numSeats + +totalSeats;
+	seatsUsed = +occupants + +seatsUsed;
+}
+
+function Activity(count, name){
+	this.count = count;
+	this.name = name;
+}
+
+$(function(){
+$('#year-select').on("change", function(){
+  var form_info = document.getElementById("choose_survey_form");
+  var month_select = document.getElementById("month-select");
+  var day_select = document.getElementById("day-select");
+  var id_select = document.getElementById("survey_id_select");
+  var survey_submit = document.getElementById("query_submit_button");
+  month_select.style.display = "inline";
+  day_select.style.display = "none";
+  id_select.style.display = "none";
+  survey_submit.style.display = "none";
+  cur_year = form_info.elements["year-select"].value;
+
+  //Get rid previous select options before repopulating
+  var length = month_select.options.length;
+  if(length > 1){
+      for(i = 0; i < length; i++){
+          month_select.remove(1);
+      }
+  }
+
+  $.ajax({
+            url: 'phpcalls/get-survey-months.php',
+            type: 'get',
+            data:{ 'selected_year': cur_year },
+            success: function(data){
+                json_object = JSON.parse(data);
+                var month_select = document.getElementById('month-select');
+      var month_name;
+
+                for(var i = 0; i < json_object.length; i++){
+                    var obj = json_object[i];
+        var month = obj[0];
+
+        switch(month){
+          case "1":
+            month_name = "January";
+            break;
+
+          case "2":
+            month_name = "February";
+            break;
+
+          case "3":
+            month_name = "March";
+            break;
+
+          case "4":
+            month_name = "April";
+            break;
+
+          case "5":
+            month_name = "May";
+            break;
+
+          case "6":
+            month_name = "June";
+            break;
+
+          case "7":
+            month_name = "July";
+            break;
+
+          case "8":
+            month_name = "August";
+            break;
+
+          case "9":
+            month_name = "September";
+            break;
+
+          case "10":
+            month_name = "October";
+            break;
+
+          case "11":
+            month_name = "November";
+            break;
+
+          defualt:
+            month_name = "December";
+            break;
+        }
+
+                    var option = document.createElement('option');
+                    option.value = obj[0];
+                    option.innerHTML = month_name;
+                    month_select.appendChild(option);
+                }
+            }
+       });
+});
+
+$('#month-select').on("change", function(){
+  var form_info = document.getElementById("choose_survey_form");
+  var day_select = document.getElementById("day-select");
+  var id_select = document.getElementById("survey_id_select");
+  var survey_submit = document.getElementById("query_submit_button");
+  day_select.style.display = "inline";
+  id_select.style.display = "none";
+  survey_submit.style.display = "none";
+
+  cur_month = form_info.elements["month-select"].value;
+
+  //Get rid previous select options before repopulating
+  var length = day_select.options.length;
+  if(length > 1){
+      for(i = 0; i < length; i++){
+          day_select.remove(1);
+      }
+  }
+
+  $.ajax({
+            url: 'phpcalls/get-survey-days.php',
+            type: 'get',
+            data:{ 'selected_year': cur_year,
+                   'selected_month': cur_month},
+            success: function(data){
+
+                //console.log("got dates");
+                json_object = JSON.parse(data);
+                var day_select = document.getElementById('day-select');
+
+                for(var i = 0; i < json_object.length; i++){
+                    var obj = json_object[i];
+
+                    var option = document.createElement('option');
+                    option.value = obj[0];
+                    option.innerHTML = obj[0];
+                    day_select.appendChild(option);
+                }
+            }
+        });
+});
+
+$('#day-select').on("change", function(){
+  var form_info = document.getElementById("choose_survey_form");
+  var id_select = document.getElementById("survey_id_select");
+  var survey_submit = document.getElementById("query_submit_button");
+  id_select.style.display = "inline";
+  survey_submit.style.display = "inline";
+  cur_day = form_info.elements["day-select"].value;
+
+  cur_selected_date = cur_year + '-' + cur_month + '-' + cur_day;
+
+  //Get rid previous select options before repopulating
+  var select = document.getElementById('survey_id_select');
+  var length = select.options.length;
+  if(length > 1){
+      for(i = 0; i < length; i++){
+          select.remove(1);
+      }
+  }
+
+  $.ajax({
+            url: 'phpcalls/get-survey-ids.php',
+            type: 'get',
+            data:{ 'selected_date': cur_selected_date },
+            success: function(data){
+                json_object = JSON.parse(data);
+                var survey_select = document.getElementById('survey_id_select');
+
+                for(var i = 0; i < json_object.length; i++){
+                    var obj = json_object[i];
+                    surv_id = obj['survey_id'];
+                    lay_id = obj['layout_id'];
+        floor_num = obj['floor'];
+        surv_date_time = obj['survey_date'];
+        surv_date_time_arr = surv_date_time.split(" ");
+        surv_time = surv_date_time_arr[1];
+                    var option = document.createElement('option');
+                    option.value = surv_id;
+                    option.innerHTML = "Survey: " + surv_id + " for Layout " + lay_id + " on floor " + floor_num + " at " + surv_time;
+                    survey_select.appendChild(option);
+                }
+            }
+        });
+});
+});
+
+function printReport(){
+var rFrame = document.getElementById('print_frame');
+rFrame.contentWindow.print();
+}
+
+function make_map(survey_id){
+  document.getElementById("query_print_button").style.display = "inline";
+
+  $.ajax({
+            url: 'phpcalls/get-survey-info.php',
+            type: 'get',
+            data:{ 'survey_id': survey_id},
+            success: function(data){
+                json_id = JSON.parse(data);
+                lay_id = json_id[0].layout_id;
+						    floor_num = json_id[0].floor;
+						    surv_date_time = json_id[0].survey_date;
+						    surv_date_time_arr = surv_date_time.split(" ");
+						    surv_time = surv_date_time_arr[1];
+						    surv_date = surv_date_time_arr[0];
+                var query_header = document.getElementById('query_header');
+              	query_header.style.display = "none";
+
+					      survey_info_legend.onAdd = function (mymap){
+						        var div = L.DomUtil.create('div', 'report_legend');
+						        var header = document.createElement('p');
+						        header.innerHTML = "Survey: " + survey_id + "</br>Layout: "
+						          + lay_id + "</br>Floor: " + floor_num
+						          + "</br> Time: " + surv_time + "</br> Date: " + surv_date;
+						        print_header = "Survey " + survey_id + " for Layout "
+						          + lay_id + " on Floor " + floor_num
+						          + " at " + surv_time + " on " + surv_date;
+						        div.appendChild(header);
+						        return div;
+      					}
+      					survey_info_legend.addTo(mymap);
+          }
+  });
+  areaMap = new Map();
+  furnMap = new Map();
+
+  //popuate furnMap and areaMap then place on map
+  populateObjs(survey_id);
+
+
+
+
+  mymap.fitBounds(bounds);
+
+
+  //On zoomend, resize the marker icons
+    mymap.on('zoomend', function() {
+        var markerSize;
+        var markerAnchor;
+        //resize the markers depending on zoomlevel so they appear to scale
+        //zoom is limited to 0-4
+        switch(mymap.getZoom()){
+            case 0: markerSize= 5; markerAnchor= -2.5; break;
+            case 1: markerSize= 10; markerAnchor = -5; break;
+            case 2: markerSize= 20; markerAnchor = -10; break;
+            case 3: markerSize= 40; markerAnchor = -20; break;
+            case 4: markerSize= 80; markerAnchor = -40; break;
+        }
+        var newzoom = '' + (markerSize) +'px';
+        var newanchor = '' + (markerAnchor) +'px';
+        var newLargeZoom = '' + (markerSize*2) +'px';
+        var newLargeAnchor = '' + (markerAnchor*2) +'px';
+        $('#mapid .furnitureIcon').css({'width':newzoom,'height':newzoom, 'margin-left':newanchor, 'margin-top':newanchor});
+        $('#mapid .furnitureLargeIcon').css({'width':newLargeZoom,'height':newLargeZoom, 'margin-left':newLargeAnchor, 'margin-top':newLargeAnchor});
+      });
+
+  mymap.on("browser-print-start", function(e){
+    /*on print start we already have a print map and we can create new control and add it to the print map to be able to print custom information */
+    survey_info_legend.addTo(e.printMap);
+  });
+
+  mymap.on("browser-print-end", function(e){
+    survey_info_legend.addTo(mymap);
+  });
+}
 
 //Populates Objects on the map
 function populateObjs(survey_id){
--- source/query-report.php
+++ source/query-report.php
@@ -17,6 +17,7 @@
     integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
     crossorigin=""></script>
 		<script src="./javascript/report-objs-pop.js"></script>
+		<script src="./javascript/report-functions.js"></script>
 		<script src="./javascript/leaflet.rotatedMarker.js"></script>
 		<script src="./javascript/icons.js"></script>
 		<script src="./javascript/leaflet.browser.print.min.js"></script>
@@ -33,197 +34,7 @@
 
 </head>
 <script type="text/javascript">
-    var cur_selected_date;
-		var cur_year;
-		var cur_month;
-		var cur_day;
-		var json_object;
-		var survey_info_legend = L.control();
 
-    $(function(){
-		$('#year-select').on("change", function(){
-			var form_info = document.getElementById("choose_survey_form");
-			var month_select = document.getElementById("month-select");
-			var day_select = document.getElementById("day-select");
-			var id_select = document.getElementById("survey_id_select");
-			var survey_submit = document.getElementById("query_submit_button");
-			month_select.style.display = "inline";
-			day_select.style.display = "none";
-			id_select.style.display = "none";
-			survey_submit.style.display = "none";
-			cur_year = form_info.elements["year-select"].value;
-
-			//Get rid previous select options before repopulating
-			var length = month_select.options.length;
-			if(length > 1){
-					for(i = 0; i < length; i++){
-							month_select.remove(1);
-					}
-			}
-
-			$.ajax({
-                url: 'phpcalls/get-survey-months.php',
-                type: 'get',
-                data:{ 'selected_year': cur_year },
-                success: function(data){
-                    json_object = JSON.parse(data);
-                    var month_select = document.getElementById('month-select');
-					var month_name;
-
-                    for(var i = 0; i < json_object.length; i++){
-                        var obj = json_object[i];
-						var month = obj[0];
-
-						switch(month){
-							case "1":
-								month_name = "January";
-								break;
-
-							case "2":
-								month_name = "February";
-								break;
-
-							case "3":
-								month_name = "March";
-								break;
-
-							case "4":
-								month_name = "April";
-								break;
-
-							case "5":
-								month_name = "May";
-								break;
-
-							case "6":
-								month_name = "June";
-								break;
-
-							case "7":
-								month_name = "July";
-								break;
-
-							case "8":
-								month_name = "August";
-								break;
-
-							case "9":
-								month_name = "September";
-								break;
-
-							case "10":
-								month_name = "October";
-								break;
-
-							case "11":
-								month_name = "November";
-								break;
-
-							defualt:
-								month_name = "December";
-								break;
-						}
-
-                        var option = document.createElement('option');
-                        option.value = obj[0];
-                        option.innerHTML = month_name;
-                        month_select.appendChild(option);
-                    }
-                }
-           });
-		});
-
-		$('#month-select').on("change", function(){
-			var form_info = document.getElementById("choose_survey_form");
-			var day_select = document.getElementById("day-select");
-			var id_select = document.getElementById("survey_id_select");
-			var survey_submit = document.getElementById("query_submit_button");
-			day_select.style.display = "inline";
-			id_select.style.display = "none";
-			survey_submit.style.display = "none";
-
-			cur_month = form_info.elements["month-select"].value;
-
-			//Get rid previous select options before repopulating
-			var length = day_select.options.length;
-			if(length > 1){
-					for(i = 0; i < length; i++){
-							day_select.remove(1);
-					}
-			}
-
-			$.ajax({
-                url: 'phpcalls/get-survey-days.php',
-                type: 'get',
-                data:{ 'selected_year': cur_year,
-							 				 'selected_month': cur_month},
-                success: function(data){
-
-                    //console.log("got dates");
-                    json_object = JSON.parse(data);
-                    var day_select = document.getElementById('day-select');
-
-                    for(var i = 0; i < json_object.length; i++){
-                        var obj = json_object[i];
-
-                        var option = document.createElement('option');
-                        option.value = obj[0];
-                        option.innerHTML = obj[0];
-                        day_select.appendChild(option);
-                    }
-                }
-            });
-		});
-
-		$('#day-select').on("change", function(){
-			var form_info = document.getElementById("choose_survey_form");
-			var id_select = document.getElementById("survey_id_select");
-			var survey_submit = document.getElementById("query_submit_button");
-			id_select.style.display = "inline";
-			survey_submit.style.display = "inline";
-			cur_day = form_info.elements["day-select"].value;
-
-			cur_selected_date = cur_year + '-' + cur_month + '-' + cur_day;
-
-			//Get rid previous select options before repopulating
-			var select = document.getElementById('survey_id_select');
-			var length = select.options.length;
-			if(length > 1){
-					for(i = 0; i < length; i++){
-							select.remove(1);
-					}
-			}
-
-			$.ajax({
-                url: 'phpcalls/get-survey-ids.php',
-                type: 'get',
-                data:{ 'selected_date': cur_selected_date },
-                success: function(data){
-                    json_object = JSON.parse(data);
-                    var survey_select = document.getElementById('survey_id_select');
-
-                    for(var i = 0; i < json_object.length; i++){
-                        var obj = json_object[i];
-                        surv_id = obj['survey_id'];
-                        lay_id = obj['layout_id'];
-						floor_num = obj['floor'];
-						surv_date_time = obj['survey_date'];
-						surv_date_time_arr = surv_date_time.split(" ");
-						surv_time = surv_date_time_arr[1];
-                        var option = document.createElement('option');
-                        option.value = surv_id;
-                        option.innerHTML = "Survey: " + surv_id + " for Layout " + lay_id + " on floor " + floor_num + " at " + surv_time;
-                        survey_select.appendChild(option);
-                    }
-                }
-            });
-		});
-    });
-
-	function printReport(){
-		var rFrame = document.getElementById('print_frame');
-		rFrame.contentWindow.print();
-	}
 </script>
 <body>
     <header>
@@ -288,89 +99,14 @@
 		if (array_key_exists("survey_id", $_GET)){
 			?>
 			<script>
-
 				//create maps and grab survey_id
 				var survey_id = <?= $_GET["survey_id"] ?>;
-
 				//used to keep track of all the seats on the floor and how many are being used
-				var totalSeats = 0;
-				var seatsUsed = 0;
-				var print_header;
-				var area_string;
-				var modified_furn = 0;
-
-				document.getElementById("query_print_button").style.display = "inline";
-
-				$.ajax({
-	                url: 'phpcalls/get-survey-info.php',
-	                type: 'get',
-	                data:{ 'survey_id': survey_id},
-	                success: function(data){
-	                    json_id = JSON.parse(data);
-	                    lay_id = json_id[0].layout_id;
-						floor_num = json_id[0].floor;
-						surv_date_time = json_id[0].survey_date;
-						surv_date_time_arr = surv_date_time.split(" ");
-						surv_time = surv_date_time_arr[1];
-						surv_date = surv_date_time_arr[0];
-	                    var query_header = document.getElementById('query_header');
-	                	query_header.style.display = "none";
-
-						survey_info_legend.onAdd = function (mymap){
-							var div = L.DomUtil.create('div', 'report_legend');
-							var header = document.createElement('p');
-							header.innerHTML = "Survey: " + survey_id + "</br>Layout: "
-								+ lay_id + "</br>Floor: " + floor_num
-								+ "</br> Time: " + surv_time + "</br> Date: " + surv_date;
-							print_header = "Survey " + survey_id + " for Layout "
-								+ lay_id + " on Floor " + floor_num
-								+ " at " + surv_time + " on " + surv_date;
-							div.appendChild(header);
-							return div;
-						}
-						survey_info_legend.addTo(mymap);
-	                }
-	            });
-				areaMap = new Map();
-				furnMap = new Map();
-
-				//define objects
-				function Area(area_id, verts, area_name){
-					this.area_id = area_id;
-					this.verts = verts;
-					this.area_name = area_name;
-					this.occupants = 0;
-					this.seats = 0;
-				}
-
-				function Verts(x, y, order){
-					this.x = x;
-					this.y = y;
-					this.order = order;
-				}
-
-				function Furniture(fid, numSeats, x, y, degreeOffset, ftype, inArea, occupants, activities){
-					this.fid = fid;
-					this.numSeats = numSeats;
-					this.x = x;
-					this.y = y;
-					this.degreeOffset = degreeOffset;
-					this.ftype = ftype;
-					this.inArea = inArea;
-					this.occupants = occupants;
-					this.activities = activities;
-					totalSeats = +numSeats + +totalSeats;
-					seatsUsed = +occupants + +seatsUsed;
-				}
-
-				function Activity(count, name){
-					this.count = count;
-					this.name = name;
-				}
-
-				//popuate furnMap and areaMap then place on map
-				populateObjs(survey_id);
-
+			  var totalSeats = 0;
+			  var seatsUsed = 0;
+			  var print_header;
+			  var area_string;
+			  var modified_furn = 0;
 
 				//make map
 				var mymap = L.map('mapid', {crs: L.CRS.Simple});
@@ -378,38 +114,7 @@
 				var furnitureLayer = L.layerGroup().addTo(mymap);
 				var bounds = [[0,0], [360,550]];
 
-				mymap.fitBounds(bounds);
-
-
-				//On zoomend, resize the marker icons
-		   		mymap.on('zoomend', function() {
-			        var markerSize;
-			        var markerAnchor;
-			        //resize the markers depending on zoomlevel so they appear to scale
-			        //zoom is limited to 0-4
-			        switch(mymap.getZoom()){
-			            case 0: markerSize= 5; markerAnchor= -2.5; break;
-			            case 1: markerSize= 10; markerAnchor = -5; break;
-			            case 2: markerSize= 20; markerAnchor = -10; break;
-			            case 3: markerSize= 40; markerAnchor = -20; break;
-			            case 4: markerSize= 80; markerAnchor = -40; break;
-			        }
-			        var newzoom = '' + (markerSize) +'px';
-			        var newanchor = '' + (markerAnchor) +'px';
-			        var newLargeZoom = '' + (markerSize*2) +'px';
-			        var newLargeAnchor = '' + (markerAnchor*2) +'px';
-			        $('#mapid .furnitureIcon').css({'width':newzoom,'height':newzoom, 'margin-left':newanchor, 'margin-top':newanchor});
-			        $('#mapid .furnitureLargeIcon').css({'width':newLargeZoom,'height':newLargeZoom, 'margin-left':newLargeAnchor, 'margin-top':newLargeAnchor});
-        		});
-
-				mymap.on("browser-print-start", function(e){
-					/*on print start we already have a print map and we can create new control and add it to the print map to be able to print custom information */
-					survey_info_legend.addTo(e.printMap);
-				});
-
-				mymap.on("browser-print-end", function(e){
-					survey_info_legend.addTo(mymap);
-        });
+				make_map(survey_id);
 
 		</script>
 		<?php
